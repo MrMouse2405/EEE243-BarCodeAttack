56ab46e35704dc5adc77432bbe84a8f1e7a37c40
MrMouse2405 <68935395+MrMouse2405@users.noreply.github.com>
2024-10-23 17:07:11 -0400

    Added Logic for Detecting (needs more fine tuning)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 /*
-* The robot will follow black lines on the floor using its
-* sensors while displaying "GO!!!". Once it reaches the end,
-* it will turn around and follow the track again.
-*
-* Author: OCdt Syed and OCdt Gratton
-* Version: 1 October 2024
-*/
+ * The robot will follow black lines on the floor using its
+ * sensors while displaying "GO!!!". Once it reaches the end,
+ * it will turn around and follow the track again.
+ *
+ * Author: OCdt Syed and OCdt Gratton
+ * Version: 1 October 2024
+ */
 
 #include <Pololu3piPlus32U4.h>
 
@@ -26,11 +26,11 @@ ButtonC buttonC;
 // Sensor Constants
 #define NUM_SENSORS 5
 #define NUM_SENSORS_START 1 // sensor 2
-#define NUM_SENSORS_END  3 // sensor 4
+#define NUM_SENSORS_END 3   // sensor 4
 
 // Encoder Sensors
 #define ENCODER_SENSOR_RIGHT 4 // sensor 5
-#define ENCODER_SENSOR_LEFT  0 // sensor 1
+#define ENCODER_SENSOR_LEFT 0  // sensor 1
 
 // time between each IR sample
 #define LINE_SENSOR_TIMEOUT 2000
@@ -40,10 +40,10 @@ ButtonC buttonC;
 
 // Values below this will be considered to
 // be not on black line.
-#define LINE_THRESHOLD  200
+#define LINE_THRESHOLD 200
 
 // Maximum & Minimum speed the motors will be allowed to turn.
-#define MAX_SPEED 125
+#define MAX_SPEED 200
 #define MIN_SPEED 0
 
 // Speed the motors will run when centered on the line.
@@ -61,9 +61,9 @@ typedef uint64_t MilliSeconds;
  * This configuration uses a default proportional constant of 1/4
  * and a derivative constant of 1, which appears to perform well at low speeds.
  * Note: Adapted from Pololu3piplus documentation.
-*/
+ */
 #define PROPORTIONAL_CONSTANT 64 // coefficient of the P term * 256
-#define DERIVATIVE_CONSTANT 256 // coefficient of the D term * 256
+#define DERIVATIVE_CONSTANT 256  // coefficient of the D term * 256
 
 /*
  *
@@ -71,32 +71,38 @@ typedef uint64_t MilliSeconds;
  *
  */
 
-#define PLAY_GO(buzzer)   buzzer.play("L16 cdegreg4"); while (buzzer.isPlaying());
-#define PLAY_BEEP(buzzer) buzzer.play(">g32>>c32"); while (buzzer.isPlaying());
-
+#define PLAY_GO(buzzer)          \
+    buzzer.play("L16 cdegreg4"); \
+    while (buzzer.isPlaying())   \
+        ;
+#define PLAY_BEEP(buzzer)      \
+    buzzer.play(">g32>>c32");  \
+    while (buzzer.isPlaying()) \
+        ;
 
 /*
-* Displays a string centered along the y-axis of the display.
-*
-* string: the string to be displayed
-* line: which number line to be displayed on. (0 <= line < 8)
-*/
+ * Displays a string centered along the y-axis of the display.
+ *
+ * string: the string to be displayed
+ * line: which number line to be displayed on. (0 <= line < 8)
+ */
 
-void display_centered(const String &s, const uint8_t line) {
+void display_centered(const String &s, const uint8_t line)
+{
     // 10 is half of 21 (see function setup)
     display.gotoXY(10 - (s.length() / 2), line);
     display.print(s.c_str());
 }
 
-
 /*
-* Calibrates the sensors by reading values as the robot turns,
-* comparing these to previous readings, and setting the highest value
-* to 1000 and the lowest to zero.
-*
-* Takes no parameters and returns no values.
-*/
-void calibrateSensors() {
+ * Calibrates the sensors by reading values as the robot turns,
+ * comparing these to previous readings, and setting the highest value
+ * to 1000 and the lowest to zero.
+ *
+ * Takes no parameters and returns no values.
+ */
+void calibrateSensors()
+{
     ledRed(true);
     ledYellow(true);
     display.clear();
@@ -107,15 +113,24 @@ void calibrateSensors() {
 
     // turn left
     Motors::setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
-    for (int i = 0; i <= 40; i++) { lineSensors.calibrate(); }
+    for (int i = 0; i <= 40; i++)
+    {
+        lineSensors.calibrate();
+    }
 
     // turn all the way to the right
-    Motors::setSpeeds(-(int16_t) CALIBRATION_SPEED, CALIBRATION_SPEED);
-    for (int i = 0; i <= 80; i++) { lineSensors.calibrate(); };
+    Motors::setSpeeds(-(int16_t)CALIBRATION_SPEED, CALIBRATION_SPEED);
+    for (int i = 0; i <= 80; i++)
+    {
+        lineSensors.calibrate();
+    };
 
     // turn back to center
     Motors::setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
-    for (int i = 0; i <= 40; i++) { lineSensors.calibrate(); }
+    for (int i = 0; i <= 40; i++)
+    {
+        lineSensors.calibrate();
+    }
 
     // stop
     Motors::setSpeeds(0, 0);
@@ -135,20 +150,23 @@ void calibrateSensors() {
  *  Ideal for handling optional parameters or return values safely.
  */
 
-template<typename T>
-class Option {
+template <typename T>
+class Option
+{
     bool result;
     T value;
 
 public:
     // constructor with a valid value
-    explicit Option(const T value) {
+    explicit Option(const T value)
+    {
         this->result = true;
         this->value = value;
     }
 
     // empty value constructor
-    explicit Option() {
+    explicit Option()
+    {
         this->result = false;
     }
 
@@ -159,22 +177,34 @@ public:
     T get_value() const { return this->value; }
 };
 
+bool isBarcodeDetected(uint16_t lineSensorValues[NUM_SENSORS])
+{
+    return (lineSensorValues[ENCODER_SENSOR_LEFT] > LINE_THRESHOLD) && (lineSensorValues[ENCODER_SENSOR_RIGHT] > LINE_THRESHOLD);
+}
 
 /*
-* Assesses whether the robot's sensors detect the line
-* and calculates the weighted average of the values obtained
-* from the line sensors.
-*
-* Returns Option<int16_t>.
-*
-* If robot's sensors detect the line:
-*   Option<int16_t> will have a value (weighted avg of 3 central IR sensors).
-*
-* If robot's sensors do not detect the line:
-*   Option<int16_t> will be empty.
-*/
+ * Assesses whether the robot's sensors detect the line
+ * and calculates the weighted average of the values obtained
+ * from the line sensors.
+ *
+ * Returns Option<int16_t>.
+ *
+ * If robot's sensors detect the line:
+ *   Option<int16_t> will have a value (weighted avg of 3 central IR sensors).
+ *
+ * If robot's sensors do not detect the line:
+ *   Option<int16_t> will be empty.
+ */
 
-Option<uint16_t> readLineBlack() {
+typedef struct
+{
+    uint16_t LineSensorDetectionResult;
+    bool IsBarcodeSensorDetected;
+
+} SensorResults;
+
+Option<SensorResults> detectLines()
+{
     bool onLine = false;
     uint32_t avg = 0; // this is for the weighted total
     uint16_t sum = 0; // this is for the denominator, which is <= 64000
@@ -183,117 +213,73 @@ Option<uint16_t> readLineBlack() {
     static uint16_t lineSensorValues[NUM_SENSORS];
     lineSensors.readCalibrated(lineSensorValues);
 
-    for (uint8_t i = NUM_SENSORS_START; i <= NUM_SENSORS_END; i++) {
+    for (uint8_t i = NUM_SENSORS_START; i <= NUM_SENSORS_END; i++)
+    {
         const uint16_t value = lineSensorValues[i];
 
         // keep track of whether we see the line at all
-        if (value > LINE_THRESHOLD) { onLine = true; }
-
+        if (value > LINE_THRESHOLD)
+        {
+            onLine = true;
+        }
 
         // only average in values that are above a noise threshold
-        if (value > NOISE_THRESHOLD) {
+        if (value > NOISE_THRESHOLD)
+        {
             avg += static_cast<uint32_t>(value) * (i * 1000);
             sum += value;
         }
     }
 
-
-    if (!onLine) {
+    if (!onLine)
+    {
         // if reached end (None of them see line)
-        if (sum == 0) {
-            return Option<uint16_t>();
+        if (sum == 0)
+        {
+            return Option<SensorResults>();
         }
-
 
         // If it last read to the left of center, return 0.
-        if (lastPosition < (NUM_SENSORS - 3) * 1000 / 2) {
-            return Option<uint16_t>{0};
+        if (lastPosition < (NUM_SENSORS - 3) * 1000 / 2)
+        {
+            return Option<SensorResults>{{0, isBarcodeDetected(lineSensorValues)}};
         }
         // If it last read to the right of center, return the max.
-        return Option<uint16_t>{(NUM_SENSORS - 1) * 1000};
+        return Option<SensorResults>{{(NUM_SENSORS - 1) * 1000, isBarcodeDetected(lineSensorValues)}};
     }
-
 
     lastPosition = avg / sum;
-    return Option<uint16_t>{lastPosition};
+    return Option<SensorResults>{{lastPosition, isBarcodeDetected(lineSensorValues)}};
 }
 
-
-/*
-* Checks if at least one of the three IR sensors is on the line
-*/
-
-bool isOnLine() {
-    static uint16_t lineSensorValues[NUM_SENSORS];
-    lineSensors.readCalibrated(lineSensorValues);
-    
-    for (uint8_t i = NUM_SENSORS_START; i <= NUM_SENSORS_END; i++) {
-        if (lineSensorValues[i] > LINE_THRESHOLD) {
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-/*
- * Checks if both encoder sensors
- * are currently detecting an encoder line.
- *
- * Returns true if BOTH are currently detecting a line
- * Otherwise returns false
- *
- */
-
-bool isBarcodeDetected() {
-    static uint16_t lineSensorValues[NUM_SENSORS];
-    lineSensors.readCalibrated(lineSensorValues);
-
-    return (lineSensorValues[ENCODER_SENSOR_LEFT] > LINE_THRESHOLD) && (
-               lineSensorValues[ENCODER_SENSOR_RIGHT] > LINE_THRESHOLD);
-}
-
-/*
- *
- * Adjusts the motors to move in the direction of line.
- *
- * This function returns:
- *  If robot still detects black line to follow: true
- *  If robot does not detect black line to follow: false
- *
- */
-
-bool follow() {
+bool follow(const Option<SensorResults> *optionalPosition)
+{
     static int16_t lastError = 0;
     // Get the position of the line.
-    const Option<uint16_t> optionalPosition = readLineBlack();
 
-    if (optionalPosition.is_absent()) {
+    if (optionalPosition->is_absent())
+    {
         Motors::setSpeeds(0, 0);
         return false;
     }
 
-    const int position = static_cast<int>(optionalPosition.get_value());
+    const int position = static_cast<int>(optionalPosition->get_value().LineSensorDetectionResult);
 
     // Our "error" is how far we are away from the center of the
     // line, which corresponds to position 2000.
     const int error = position - 2000;
-
 
     // Get motor speed difference using PROPORTIONAL_CONSTANT and derivative
     // PID terms (the integral term is generally not very useful
     // for line following).
     const int speedDifference = error * PROPORTIONAL_CONSTANT / 256 + (error - lastError) * DERIVATIVE_CONSTANT / 256;
 
-
     lastError = error;
-
 
     // Get individual motor speeds.  The sign of speedDifference
     // determines if the robot turns left or right.
     int leftSpeed = BASE_SPEED + speedDifference;
     int rightSpeed = BASE_SPEED - speedDifference;
-
 
     // Constrain our motor speeds to be between 0 and MAX_SPEED.
     // One motor will always be turning at MAX_SPEED, and the other
@@ -309,24 +295,113 @@ bool follow() {
     return true;
 }
 
-/*
-* Sets up the display to be in the proper formatting
-*/
+typedef enum States
+{
+    START,
+    NOT_DETECTED,
+    DETECTED,
+} State;
 
-void setup() {
+typedef class StateMachine
+{
+
+    State state = START;
+    MilliSeconds t0;
+
+    int barcodeResults[100];
+    int len = 0;
+
+public:
+    StateMachine()
+    {
+    }
+
+    State getState()
+    {
+        return state;
+    }
+
+    int *getResults()
+    {
+        return barcodeResults;
+    }
+
+    int getResultLen()
+    {
+        return len;
+    }
+
+    void push(State newState)
+    {
+
+        if (newState == state)
+        {
+            return;
+        }
+
+        switch (state)
+        {
+        case START:
+            state = DETECTED;
+            t0 = millis();
+            break;
+        case NOT_DETECTED:
+            state = DETECTED;
+            barcodeResults[len] = millis() - t0;
+            len++;
+            t0 = millis();
+            break;
+        case DETECTED:
+            state = NOT_DETECTED;
+            barcodeResults[len] = millis() - t0;
+            len++;
+            t0 = millis();
+            break;
+        }
+    }
+
+} BarCodeStateMachine;
+
+void followAndReadBarCode(BarCodeStateMachine *stateMachine)
+{
+    for (;;)
+    {
+        const Option<SensorResults> results = detectLines();
+        const bool onLine = follow(&results);
+        if (!onLine)
+        {
+            return;
+        }
+        if (results.get_value().IsBarcodeSensorDetected)
+        {
+            stateMachine->push(DETECTED);
+        }
+        else
+        {
+            stateMachine->push(NOT_DETECTED);
+        }
+    }
+}
+
+/*
+ * Sets up the display to be in the proper formatting
+ */
+
+void setup()
+{
     display.setLayout21x8();
     lineSensors.setTimeout(LINE_SENSOR_TIMEOUT);
 }
 
-
-void loop() {
+void loop()
+{
     /*
      *
      *  Welcome Screen
      *
      */
     display_centered("Abdul Mannan Syed", 0);
-    display_centered("Gratton", 1);
+    display_centered("Nathan Gratton", 1);
     display_centered("Lab 4: Barcode", 4);
     display_centered("To start, press B", 7);
     buttonB.waitForButton();
@@ -339,8 +414,8 @@ void loop() {
      */
 
     calibrateSensors();
-    display_centered("Ready",1);
-    display_centered("<  GO  >",4);
+    display_centered("Ready", 1);
+    display_centered("<  GO  >", 4);
     buttonB.waitForButton();
 
     /*
@@ -351,7 +426,17 @@ void loop() {
     display_centered("GO!!!", 4);
     PLAY_GO(buzzer);
 
+    /*
+     *
+     * Follow Line
+     *
+     * Read & Store Detections
+     *
+     */
 
+    BarCodeStateMachine stateMachine = BarCodeStateMachine();
+    followAndReadBarCode(&stateMachine);
+    PLAY_BEEP(buzzer);
+    display_centered("Values:" + String(stateMachine.getResultLen()), 4);
+    buttonB.waitForButton();
 }
-
-// jrdonat
