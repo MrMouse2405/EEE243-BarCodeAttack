add286c3e6b8b5fffcb9cea757674ce738486a42
MrMouse2405 <68935395+MrMouse2405@users.noreply.github.com>
2024-11-06 02:13:55 -0500

    Refactored

---
+++ src/LineFollowing.h
@@ -0,0 +1,60 @@
+#pragma once
+
+namespace LineFollowing {
+    typedef enum LFStates
+    {
+        // Robot is ready for action
+        Initialized,
+        // Robot is calibrating IR sensors
+        // No actions will be accepted
+        Calibrating,
+        // Robot is calibrated and ready
+        Ready,
+        // Currently following the line
+        Following,
+        // Stopped by caller
+        ForcedStop,
+        // Robot has reached the end of line
+        ReachedEnd,
+    } LineFollowingStates;
+
+    typedef enum LFActions {
+        // calibrate the IR sensors
+        Calibrate,
+        // start the line follower algorithm
+        Start,
+        // force stop the line follow algorithm
+        Stop,
+    } LineFollowingActions;
+
+    class LineFollower {
+    private:
+         // Tracks the state of Line Follower
+        LineFollowingStates state = Initialized;
+        void followLine();
+    public:
+        LineFollower();
+        ~LineFollower();
+        /**
+         * This should be called in a loop while the robot
+         * is allowed to move
+         * 
+         */
+        void follow();
+        /**
+         * 
+         *  Push user defined actions
+         * 
+         *  i.e give commands to start / stop
+         *  the algorithm
+         * 
+        */
+        void pushAction(LFActions action);
+        /**
+         * 
+         *  Get the current state of algorithm
+         * 
+         */
+        LineFollowingStates getState();
+    };
+}
---
+++ src/Sensors.cpp
@@ -0,0 +1,140 @@
+/** 
+ * Sensors
+ * 
+ * Responsible for calibrating and obtaining
+ * values from the environment via the IR sensors.
+ * 
+ * Author: OCdt Syed
+ * Date: 2024-10-31
+ * 
+*/
+
+#include "Sensors.h"
+
+namespace Sensors {
+    Pololu3piPlus32U4::LineSensors lineSensors;
+    // values read from the sensors will be stored here
+    static uint16_t lineSensorValues[NUM_SENSORS];
+}
+
+
+/*
+ * Calibrates the sensors by reading values as the robot turns,
+ * comparing these to previous readings, and setting the highest value
+ * to 1000 and the lowest to zero.
+ *
+ * Takes no parameters and returns no values.
+ */
+void Sensors::calibrateSensors()
+{
+    using namespace Pololu3piPlus32U4;
+
+    ledRed(true);
+    ledYellow(true);
+
+    // Wait 1 second and then begin automatic sensor calibration
+    // by rotating in place to sweep the sensors over the line
+    delay(1000);
+
+    // turn left
+    Motors::setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
+    for (int i = 0; i <= 40; i++)
+    {
+        Sensors::lineSensors.calibrate();
+    }
+
+    // turn all the way to the right
+    Motors::setSpeeds(-(int16_t)CALIBRATION_SPEED, CALIBRATION_SPEED);
+    for (int i = 0; i <= 80; i++)
+    {
+        Sensors::lineSensors.calibrate();
+    };
+
+    // turn back to center
+    Motors::setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
+    for (int i = 0; i <= 40; i++)
+    {
+        Sensors::lineSensors.calibrate();
+    }
+
+    // stop
+    Motors::setSpeeds(0, 0);
+    ledRed(false);
+    ledYellow(false);
+}
+
+/*
+ * Determines if both Left and Right IR Sensors have detected
+ * Barcode
+ *
+ * Returns bool
+ *
+ * bool == true if black color is detected
+ * bool == false if white color detected
+ */
+bool Sensors::isBarcodeDetected()
+{
+    return (Sensors::lineSensorValues[BARCODE_SENSOR_LEFT] > LINE_THRESHOLD) && (Sensors::lineSensorValues[BARCODE_SENSOR_RIGHT] > LINE_THRESHOLD);
+}
+
+/*
+ * Assesses whether the robot's sensors detect the line
+ * and calculates the weighted average of the values obtained
+ * from the line sensors.
+ *
+ * Returns Option<int16_t>.
+ *
+ * If robot's sensors detect the line:
+ *   Option<int16_t> will have a value (weighted avg of 3 central IR sensors).
+ *
+ * If robot's sensors do not detect the line:
+ *   Option<int16_t> will be empty.
+ */
+
+Lab4::Option<int16_t> Sensors::detectLines()
+{
+    bool onLine = false;
+    uint32_t avg = 0; // this is for the weighted total
+    uint16_t sum = 0; // this is for the denominator, which is <= 64000
+    static uint16_t lastPosition = 0;
+
+    Sensors::lineSensors.readCalibrated(Sensors::lineSensorValues);
+
+    for (uint8_t i = NUM_SENSORS_START; i <= NUM_SENSORS_END; i++)
+    {
+        const uint16_t value = Sensors::lineSensorValues[i];
+
+        // keep track of whether we see the line at all
+        if (value > LINE_THRESHOLD)
+        {
+            onLine = true;
+        }
+
+        // only average in values that are above a noise threshold
+        if (value > NOISE_THRESHOLD)
+        {
+            avg += static_cast<uint32_t>(value) * (i * 1000);
+            sum += value;
+        }
+    }
+
+    if (!onLine)
+    {
+        // if reached end (None of them see line)
+        if (sum == 0)
+        {
+            return Lab4::Option<int16_t>();
+        }
+
+        // If it last read to the left of center, return 0.
+        if (lastPosition < (NUM_SENSORS - 3) * 1000 / 2)
+        {
+            return Lab4::Option<int16_t>{0};
+        }
+        // If it last read to the right of center, return the max.
+        return Lab4::Option<int16_t>{(NUM_SENSORS - 1) * 1000};
+    }
+
+    lastPosition = avg / sum;
+    return Lab4::Option<int16_t>{static_cast<int16_t>(lastPosition)};
+}
